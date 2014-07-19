d02ff459c13682a3a5253c12c2046bb9cbb4d203
Steven Devine <stevenmichaeldevine@gmail.com>
2014-07-18 19:20:52 -0500

    skeleton working:

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+
+  config.time_zone = "Central Time (US & Canada)"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
